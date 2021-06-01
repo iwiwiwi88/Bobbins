5777538541bed8e03a0550124ecb1fd29ca34401
Iwona Sasak <sasak.iwona.88@gmail.com>
2021-06-01 20:40:51 +0200

    Initialize spring boot

---
+++ src/main/java/pl/iwi/floss/bobins/BobinsApplication.java
@@ -0,0 +1,13 @@
+package pl.iwi.floss.bobins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/iwi/floss/bobins/BobinsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.iwi.floss.bobins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
